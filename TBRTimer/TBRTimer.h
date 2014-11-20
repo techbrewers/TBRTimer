5222117e6effffef9250314930f058311098f2b4
Luciano Marisi <luciano@theappbusiness.com>
2014-11-20 11:53:22 +0000

    Added TBRTimer class with example

---
+++ TBRTimer/TBRTimer.h
@@ -0,0 +1,13 @@
+//
+//  TBRTimer.h
+//  TBRTimerExample
+//
+//  Created by Luciano Marisi on 20/11/2014.
+//  Copyright (c) 2014 TechBrewers LTD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TBRTimer : NSObject
+
+@end
---
+++ TBRTimer/TBRTimer.m
@@ -0,0 +1,13 @@
+//
+//  TBRTimer.m
+//  TBRTimerExample
+//
+//  Created by Luciano Marisi on 20/11/2014.
+//  Copyright (c) 2014 TechBrewers LTD. All rights reserved.
+//
+
+#import "TBRTimer.h"
+
+@implementation TBRTimer
+
+@end
